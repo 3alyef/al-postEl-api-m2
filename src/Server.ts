630440c4301634eceb5d506e0b1d3870ef77c55a
3alyef <3alyef.dev@gmail.com>
2024-03-29 10:07:20 -0300

    update on ALLOW

--- src/Server.ts
+++ src/Server.ts
@@ -3,13 +3,15 @@ import express, { Request, Response } from "express";
 import { router } from "./Routes/Routes";
 import { createServer, Server as ServerHTTP } from 'http';
 import { Server as Io } from "socket.io";
-const cors = require('cors');
-const ALLOW = process.env.ACESS_ALLOW_ORIGIN || "localhost"
+const cors = require("cors");
 class Server {
+    
     public app: express.Application;
     public server: ServerHTTP;
     private socketIo: Io;
+    private ALLOW: string;
     constructor(){
+        this.ALLOW = process.env.ACCESS_ALLOW_ORIGIN || "nothing";
         this.app = express();
         this.server = createServer(this.app);
         this.socketIo = new Io(this.server, {
@@ -30,8 +32,8 @@ class Server {
     private cors(): void {
         this.app.use((req: Request, res: Response, next)=>{
 
-            res.header("Access-Control-Allow-Origin", /*ALLOW*/ "*");
-            res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+            res.header("Access-Control-Allow-Origin", this.ALLOW );
+            res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
             this.app.use(cors());
             next();
         })
