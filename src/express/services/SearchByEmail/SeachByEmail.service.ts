812479839d39ff7f78d86e0db271150393a8addd
3alyef <3alyef.dev@gmail.com>
2024-05-07 12:46:20 -0300

    update

--- src/express/services/SearchByEmail/SeachByEmail.service.ts
+++ src/express/services/SearchByEmail/SeachByEmail.service.ts
@@ -45,20 +45,26 @@ class SearchByEmail {
         }
     }
 
-    private async findUser(email: string): Promise<{ userSoul: string, first_name: string } | {message: string}> {
+    private async findUser(email: string, retry: boolean = true): Promise<{ userSoul: string, first_name: string } | {message: string}> {
         try {
             const user: { soulName: string, first_name: string } | null = await userModel.findOne({ email }, 'soulName first_name');
     
             if (!user) {
-                throw {message: "Erro ao buscar usuário"}
+                return {message: "usuário não encontrado na base de dados"}; // Retorna null se o usuário não for encontrado
             }
-
+    
             // Retorna um objeto contendo o soulName do usuário
-            return {userSoul: user.soulName, first_name: user.first_name}
+            return { userSoul: user.soulName, first_name: user.first_name };
         } catch (error) {
-            const err = error as {message: string}
-            console.error('Erro ao encontrar usuário:', error);
-            return {message: err.message}
+            if (retry) {
+                // Se ocorrer um erro, e retry for verdadeiro, tenta novamente
+                console.error('Erro ao encontrar usuário:', error);
+                return this.findUser(email, false); // Tenta novamente sem repetir
+            } else {
+                const err = error as { message: string };
+                console.error('Erro ao encontrar usuário após tentativas:', error);
+                return { message: err.message };
+            }
         }
     }
 
