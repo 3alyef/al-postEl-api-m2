4ba8cc554322a061341732bab5ee0024c9908c7b
Alef A. Araujo <3alyef.dev@gmail.com>
2024-07-31 22:53:02 -0300

    up

--- src/m3_server/services/UpdateMsgStatus/UpdateMsgStatus.service.ts
+++ src/m3_server/services/UpdateMsgStatus/UpdateMsgStatus.service.ts
@@ -6,7 +6,7 @@ export async function UpdateMsgStatus(req: Request<{body: msgsUpdateStatusReques
     try { 
         const {fromUser, toUser, viewStatus, createdIn}: msgsUpdateStatusRequest = req.body;
         const newMsg = await updateStatusMsg({fromUser, toUser, viewStatus, createdIn});
-        console.log({fromUser, toUser, viewStatus, createdIn})
+        //console.log({fromUser, toUser, viewStatus, createdIn})
         res.status(200).json(newMsg).end();
     } catch(error){
         console.log("Error: "+error);
@@ -26,13 +26,13 @@ async function updateStatusMsg({fromUser, toUser, createdIn, viewStatus}: msgsUp
         )
 
         if (result.modifiedCount === 0) {
-            throw new Error("Nenhuma mensagem encontrada para atualizar com o createdIn especificado.");
+            throw {msg: "Nenhuma mensagem encontrada para atualizar com o createdIn especificado."}
         }
 
         return { success: true, message: "Status da mensagem atualizado com sucesso." };
 
-    } catch (error) {
-        throw new Error("Ocorreu um erro ao atualizar o status da mensagem no database: " + error);
-
+    } catch (error ) {
+        let errorMsg = error as {msg: string};
+        throw new Error("Ocorreu um erro ao atualizar o status da mensagem no database: " + errorMsg.msg); 
     }
 }
--- src/test/m3/services/UpdateMsgStatus.test.ts
+++ src/test/m3/services/UpdateMsgStatus.test.ts
@@ -4,17 +4,44 @@ import { messageModel } from "../../../m3_server/db/models/Models";
 jest.mock("../../../m3_server/db/models/Models.ts");
 
 describe("UpdateMsgStatus", ()=>{
-    it("Isso deve mudar o status de visualização da mensagem", async ()=>{
+    const msgSend = {
+        fromUser: "rukh0test1",
+        toUser: "rukh1test2",
+        createdIn: "2023-07-31T12:00:00Z",
+        viewStatus: "delivered"
+    }
+    it("Isso deve retornar status 200", async ()=>{
         const mockUpdateResult = {modifiedCount: 1};
 
         (messageModel.updateOne as jest.Mock).mockResolvedValue(mockUpdateResult);
-        const response = await request(app).post("/statusMsgUpdate").send({
-            fromUser: "rukh0test1",
-            toUser: "rukh1test2",
-            createdIn: "2023-07-31T12:00:00Z",
-            viewStatus: "delivered"
-        })
 
-        expect(response.status).toBe(200)
+        
+        const response = await request(app).post("/statusMsgUpdate").send(msgSend)
+
+        expect(response.status).toBe(200);
+
+        expect(response.body).toEqual({ success: true, message: "Status da mensagem atualizado com sucesso." })
     })
+
+    it("Isso deve retornar status 501", async ()=>{
+        const mockUpdateResult = {modifiedCount: 0};
+
+        (messageModel.updateOne as jest.Mock).mockResolvedValue(mockUpdateResult);
+        
+        const response = await request(app).post("/statusMsgUpdate").send(msgSend);
+
+        expect(response.status).toBe(501);
+        expect(response.body).toEqual(null)
+    })
+
+    it("Deve retornar status 501 em caso de erro no database", async () => {
+        const mockError = new Error("Erro no database");
+
+        (messageModel.updateOne as jest.Mock).mockRejectedValue(mockError);
+
+        const response = await request(app).post("/statusMsgUpdate").send(msgSend);
+
+        expect(response.status).toBe(501);
+        expect(response.body).toEqual(null)
+    });
 })
